29d46a25aa180eee68ac55916cef8579ef53a8f9
Christian Ascone <ascone.christian@gmail.com>
2023-09-08 01:24:55 +0200

    feat: add mason-nvim-dap with php configuration

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = {
+      ensure_installed = { "php", "java" },
+      handlers = {
+        php = function(config)
+          config.adapters = {
+            type = "executable",
+            command = "node",
+            args = { "/Applications/LSPs/vscode-php-debug/out/phpDebug.js" },
+          }
+          config.configurations = {
+            {
+              type = "php",
+              request = "launch",
+              name = "Listen for Xdebug",
+              port = 9003,
+            },
+          }
+          require("mason-nvim-dap").default_setup(config) -- don't forget this!
+        end,
+      },
+    },
+  },
+}
