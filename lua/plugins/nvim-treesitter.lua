b930558784b933ec5e3fa0eb2154374986c17c35
Christian Ascone <ascone.christian@gmail.com>
2023-09-20 16:53:21 +0200

    feat: setup incremental_selection in treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,10 +5,20 @@ return {
       -- add tsx and treesitter
       vim.list_extend(opts.ensure_installed, {
         "tsx",
+        "lua",
         "typescript",
         "java",
         "php",
       })
+      opts.incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<CR>",
+          scope_incremental = "<CR>",
+          node_incremental = "<TAB>",
+          node_decremental = "<S-TAB>",
+        },
+      }
     end,
   },
 }
