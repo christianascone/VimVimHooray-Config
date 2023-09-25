8443c52928ee12499372bc9319d02de5e58150d5
Christian Ascone <ascone.christian@gmail.com>
2023-09-25 10:40:58 +0200

    feat: install vim-dadbod plugin (with ui and completion)

---
+++ lua/plugins/vim-dadbod.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+    config = function()
+      local M = {}
+
+      local function db_completion()
+        require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+      end
+
+      function M.setup()
+        vim.g.db_ui_save_location = vim.fn.stdpath("config") .. require("plenary.path").path.sep .. "db_ui"
+
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = {
+            "sql",
+          },
+          command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
+        })
+
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = {
+            "sql",
+            "mysql",
+            "plsql",
+          },
+          callback = function()
+            vim.schedule(db_completion)
+          end,
+        })
+      end
+
+      return M
+    end,
+  },
+}
