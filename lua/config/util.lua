a0c49ecedfd4c6e2f189fed0e8b5200dd911b949
Christian Ascone <ascone.christian@gmail.com>
2023-11-15 14:33:39 +0100

    refactor: add boolean paramter to callDotenv function to show updated status or not

--- lua/config/util.lua
+++ lua/config/util.lua
@@ -1,12 +1,15 @@
 local M = {}
-function M.callDotenv()
+function M.callDotenv(show_updated)
+  -- default value
+  show_updated = show_updated or true
   if vim.fn.filereadable(".env") == 1 then
     local ok, err = pcall(vim.cmd, "Dotenv .env")
     if not ok then
       vim.notify(err, vim.log.levels.ERROR)
     end
-
-    vim.cmd("Dotenv")
+    if show_updated then
+      vim.cmd("Dotenv")
+    end
   end
 end
 return M
