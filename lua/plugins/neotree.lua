ad7dee8b0886f93fb4725567ca57036dd8ac9558
Christian Ascone <ascone.christian@gmail.com>
2023-11-23 11:49:21 +0100

    chore: move neotree to right

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -11,6 +11,9 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      window = {
+        position = "right",
+      },
       filesystem = {
         filtered_items = {
           visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
