92b7daa64ffd7d4e01dabe65421c1f78c4355225
Christian Ascone <ascone.christian@gmail.com>
2024-03-23 00:51:05 +0100

    feat: add navigation with hjkl between windows

--- init.lua
+++ init.lua
@@ -332,6 +332,13 @@ vim.cmd(":hi NonText guifg=bg")
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Move to window using the <ctrl> hjkl keys
+vim.keymap.set({"n"}, "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+vim.keymap.set({"n"}, "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set({"n"}, "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+vim.keymap.set({"n"}, "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
