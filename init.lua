241ef9be2139510f1a1b8e8ffa540414edfbf4fc
Christian Ascone <ascone.christian@gmail.com>
2024-09-17 00:28:42 +0200

    style: reformat code

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ local custom_path = os.getenv("XDG_DATA_HOME")
 vim.env.XDG_DATA_HOME = custom_path
 
 -- Basic Neovim Options
-vim.g.mapleader = ' ' -- Leader as SPACE
+vim.g.mapleader = " " -- Leader as SPACE
 vim.opt.number = true -- Show line numbers
 vim.opt.relativenumber = true -- Relative line numbers
 vim.opt.tabstop = 4 -- A tab is four spaces
@@ -14,16 +14,16 @@ vim.opt.smarttab = true -- Makes tabbing smarter
 vim.opt.autoindent = true -- Good auto indent
 vim.opt.smartindent = true -- Makes indenting smart
 vim.opt.wrap = false -- No Wrap lines
-vim.opt.backspace = 'indent,eol,start' -- Allow backspace over everything in insert mode
+vim.opt.backspace = "indent,eol,start" -- Allow backspace over everything in insert mode
 vim.opt.hidden = true -- Required to keep multiple buffers open
 vim.opt.hlsearch = true -- Highlight search results
 vim.opt.incsearch = true -- Makes search act like search in modern browsers
 vim.opt.ignorecase = true -- Ignore case when searching
-vim.opt.smartcase = true -- When searching try to be smart about cases 
+vim.opt.smartcase = true -- When searching try to be smart about cases
 vim.opt.splitbelow = true -- Horizontal splits will automatically be below
 vim.opt.splitright = true -- Vertical splits will automatically be to the right
 vim.opt.termguicolors = true -- Enable 24-bit RGB color in the TUI
-vim.opt.clipboard = 'unnamedplus' -- Use system clipboard
+vim.opt.clipboard = "unnamedplus" -- Use system clipboard
 
 -- Install lazy.nvim if not already installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -43,14 +43,14 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   -- Your plugins will go here. Example:
   -- {'preservim/nerdtree', config = function() vim.cmd('nnoremap <C-n> :NERDTreeToggle<CR>') end},
-{
-  "catppuccin/nvim",
-  name = "catppuccin",
-  config = function()
-    vim.cmd.colorscheme "catppuccin"
-  end
-},
-{
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
     "supermaven-inc/supermaven-nvim",
     lazy = true,
     config = function()
@@ -62,7 +62,7 @@ require("lazy").setup({
         },
       })
     end,
-  }
+  },
 })
 
 -- Shortend for keymap
@@ -81,14 +81,14 @@ local function map(mode, lhs, rhs, opts)
 end
 
 -- Supermaven
-map('n', '<leader>cS', function() require("supermaven-nvim") end, { desc = "Enable Supermaven"})
-
-
+map("n", "<leader>cS", function()
+  require("supermaven-nvim")
+end, { desc = "Enable Supermaven" })
 
 -- Highlight code when yanking
-vim.cmd [[
+vim.cmd([[
   augroup highlight_yank
   autocmd!
   au TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=150})
   augroup END
-]]
+]])
