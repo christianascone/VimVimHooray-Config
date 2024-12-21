7a73ed4a4a7062a74b038146babbb840804bbbaa
Christian Ascone <ascone.christian@gmail.com>
2024-12-21 01:18:35 +0100

    feat: update options

--- init.lua
+++ init.lua
@@ -4,11 +4,14 @@ vim.env.XDG_DATA_HOME = custom_path
 
 -- Basic Neovim Options
 vim.g.mapleader = " " -- Leader as SPACE
+
 vim.opt.number = true -- Show line numbers
 vim.opt.relativenumber = true -- Relative line numbers
-vim.opt.tabstop = 4 -- A tab is four spaces
+vim.opt.tabstop = 2 -- Number of spaces tabs count for
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4 -- When indenting with '>', use 4 spaces width
+vim.opt.shiftwidth = 2 -- Size of an indent
+vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
+vim.opt.showmode = false -- Dont show mode since we have a statusline
 vim.opt.expandtab = true -- On pressing tab, insert 4 spaces
 vim.opt.smarttab = true -- Makes tabbing smarter
 vim.opt.autoindent = true -- Good auto indent
@@ -24,6 +27,40 @@ vim.opt.splitbelow = true -- Horizontal splits will automatically be below
 vim.opt.splitright = true -- Vertical splits will automatically be to the right
 vim.opt.termguicolors = true -- Enable 24-bit RGB color in the TUI
 vim.opt.clipboard = "unnamedplus" -- Use system clipboard
+vim.opt.confirm = true -- Confirm to save changes before exiting modified buffer
+vim.opt.cursorline = true -- Enable highlighting of the current line
+vim.opt.formatoptions = "jcroqlnt" -- tcqj
+vim.opt.grepformat = "%f:%l:%c:%m"
+vim.opt.grepprg = "rg --vimgrep"
+vim.opt.completeopt = "menu,menuone,noselect"
+vim.opt.inccommand = "nosplit" -- preview incremental substitute
+vim.opt.laststatus = 2
+vim.opt.list = true -- Show some invisible characters (tabs...
+vim.opt.mouse = "a" -- Enable mouse mode
+vim.opt.pumblend = 10 -- Popup blend
+vim.opt.scrolloff = 4 -- Lines of context
+vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
+vim.opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+vim.opt.spelllang = { "en" }
+vim.opt.splitkeep = "screen"
+vim.opt.timeoutlen = 300
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.updatetime = 200 -- Save swap file and trigger CursorHold
+vim.opt.wildmode = "longest:full,full" -- Command-line completion mode
+vim.opt.winminwidth = 5 -- Minimum window width
+vim.opt.wrap = false -- Disable line wrap
+vim.opt.fillchars = {
+  foldopen = "",
+  foldclose = "",
+  -- fold = "⸱",
+  fold = " ",
+  foldsep = " ",
+  diff = "╱",
+  eob = " ",
+}
+vim.opt.exrc = true
+vim.opt.secure = true
 
 -- Install lazy.nvim if not already installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
