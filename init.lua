c57f8b890f657b9c4dd13f703b4e0add33b5f198
Christian Ascone <ascone.christian@gmail.com>
2024-12-24 11:20:46 +0100

    feat: add indent-blankline

--- init.lua
+++ init.lua
@@ -120,6 +120,32 @@ require("lazy").setup({
     },
   },
   {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+      },
+    },
+    main = "ibl",
+  },
+  {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
     opts = {
