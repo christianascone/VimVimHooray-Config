2c4c5d8994b7a6144108c8e855a398c732b7f9da
Christian Ascone <ascone.christian@gmail.com>
2024-12-24 16:09:53 +0100

    feat: install rest.nvim plugin

--- init.lua
+++ init.lua
@@ -200,6 +200,49 @@ require("lazy").setup({
     end,
   },
   {
+    "rest-nvim/rest.nvim",
+    dependencies = { { "nvim-lua/plenary.nvim" } },
+    config = function()
+      require("rest-nvim").setup({
+        -- Open request results in a horizontal split
+        result_split_horizontal = false,
+        -- Keep the http file buffer above|left when split horizontal|vertical
+        result_split_in_place = false,
+        -- Skip SSL verification, useful for unknown certificates
+        skip_ssl_verification = false,
+        -- Encode URL before making request
+        encode_url = true,
+        -- Highlight request on run
+        highlight = {
+          enabled = true,
+          timeout = 150,
+        },
+        result = {
+          -- toggle showing URL, HTTP info, headers at top the of result window
+          show_url = true,
+          -- show the generated curl command in case you want to launch
+          -- the same request via the terminal (can be verbose)
+          show_curl_command = false,
+          show_http_info = true,
+          show_headers = true,
+          -- executables or functions for formatting response body [optional]
+          -- set them to false if you want to disable them
+          formatters = {
+            json = "jq",
+            html = function(body)
+              return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
+            end,
+          },
+        },
+        -- Jump to request line on run
+        jump_to_request = false,
+        env_file = ".env",
+        custom_dynamic_variables = {},
+        yank_dry_run = true,
+      })
+    end,
+  },
+  {
     "kristijanhusak/vim-dadbod-ui",
     dependencies = {
       { "tpope/vim-dadbod", lazy = true },
@@ -718,7 +761,19 @@ require("lazy").setup({
     config = function()
       require("nvim-treesitter.configs").setup({
         -- A list of parsers you want to ensure are installed (sync with :TSInstall)
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "python", "java", "php" },
+        ensure_installed = {
+          "c",
+          "lua",
+          "http",
+          "vim",
+          "vimdoc",
+          "query",
+          "javascript",
+          "typescript",
+          "python",
+          "java",
+          "php",
+        },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
@@ -1555,6 +1610,8 @@ map("n", "<leader>Tt", "<cmd>OverseerToggle<cr>", { desc = "Overseer Toggle" })
 map("n", "<leader>Tr", "<cmd>OverseerRun<cr>", { desc = "Overseer Run" })
 --- DBUI
 map("n", "<leader>db", "<cmd>DBUIToggle<cr>", { desc = "DBUI Toggle" })
+--- Rest
+map("n", "<leader>Rr", "<cmd>Rest run<cr>", { desc = "Rest: run" })
 
 --- Custom functions ---
 local function searchAndReplace(mode)
