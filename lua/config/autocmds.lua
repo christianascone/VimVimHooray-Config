cdd7c136346bf76ae09f27033bedb9e9fe201011
Christian Ascone <ascone.christian@gmail.com>
2024-12-28 01:13:03 +0100

    fix: autoindent for php

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -104,3 +104,12 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end
   end,
 })
+
+-- autoindent for php
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "php",
+  callback = function()
+    vim.bo.autoindent = true
+    vim.bo.smartindent = true
+  end,
+})
