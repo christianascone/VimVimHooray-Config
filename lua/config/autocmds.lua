d6a26b9b8085235e986f2fc0f5399da0ad5ab64f
Christian Ascone <ascone.christian@gmail.com>
2025-02-02 00:35:59 +0100

    fix: slow toggleterm for large buffers

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -104,3 +104,16 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end
   end,
 })
+
+-- Fixes slowness of toggleterm with large buffers
+vim.api.nvim_create_augroup("disable_folding_toggleterm", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = "disable_folding_toggleterm",
+  pattern = "toggleterm",
+  callback = function(ev)
+    local bufnr = ev.buf
+    vim.api.nvim_buf_set_option(bufnr, "foldmethod", "manual")
+    vim.api.nvim_buf_set_option(bufnr, "foldtext", "foldtext()")
+  end,
+})
