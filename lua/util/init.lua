f30d4c6f35994e0e88d8b21d59f8dcd309db4100
Christian Ascone <ascone.christian@gmail.com>
2025-02-26 10:49:43 +0100

    chore: add util function to call Dotenv

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -110,4 +110,15 @@ function M.debug(msg, opts)
   end
 end
 
+function M.callDotenv()
+  if vim.fn.filereadable(".env") == 1 then
+    local ok, err = pcall(vim.cmd, "Dotenv .env")
+    if not ok then
+      vim.notify(err, vim.log.levels.ERROR)
+    end
+
+    vim.cmd("Dotenv")
+  end
+end
+
 return M
