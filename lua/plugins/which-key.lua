0cccea642ab636029ab0529f0c0da29fb56f7d16
Christian Ascone <ascone.christian@gmail.com>
2025-02-27 10:37:01 +0100

    fix: group mappings for which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,19 +2,16 @@ return {
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer Local Keymaps (which-key)",
+    opts = {
+      spec = {
+        { "<leader>?", group = "+help" },
+        { "<leader>sn", group = "+noice" },
+        { "<leader>s", group = "+search" },
+        { "<leader>g", group = "+git" },
+        { "<leader>q", group = "+quit/session" },
+        { "<leader>t", group = "+terminal" },
+        { "<leader>c", group = "+code tools" },
       },
-      { "<leader>sn", group = "+noice" },
-      { "<leader>s", group = "+search" },
-      { "<leader>g", group = "+git" },
-      { "<leader>q", group = "+quit/session" },
-      { "<leader>t", group = "+terminal" },
     },
   },
 }
