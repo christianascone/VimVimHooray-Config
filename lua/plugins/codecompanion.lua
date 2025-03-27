3ee5a523dbec9d892e2a1cac919b97509c479ae6
Christian Ascone <ascone.christian@gmail.com>
2025-03-27 11:27:47 +0100

    feat: set claude sonnet 3.5 as model for codecompanion

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -7,17 +7,30 @@ return {
       "nvim-treesitter/nvim-treesitter",
       "nvim-telescope/telescope.nvim",
       "stevearc/dressing.nvim",
-      "github/copilot.vim" -- Required for copilot adapter. Run :Copilot setup 
+      "github/copilot.vim", -- Required for copilot adapter. Run :Copilot setup
     },
     config = function()
       require("codecompanion").setup({
         strategies = {
           chat = {
             adapter = "copilot",
+            model = "claude sonnet 3.5",
           },
           inline = {
             adapter = "copilot",
+            model = "claude sonnet 3.5",
           },
+        },
+        adapters = {
+          copilot = function()
+            return require("codecompanion.adapters").extend("copilot", {
+              schema = {
+                model = {
+                  default = "claude-3.5-sonnet",
+                },
+              },
+            })
+          end,
         },
       })
     end,
