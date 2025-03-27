1c74ff2c49521ebac052eb2ccc02c3cb2226e023
Christian Ascone <ascone.christian@gmail.com>
2025-03-27 11:27:26 +0100

    chore: specify version for jdtls in mason

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -3,7 +3,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "jdtls", "phpactor" }, -- Add your servers here
+        ensure_installed = { "lua_ls", "jdtls@v1.43.0", "phpactor" }, -- Add your servers here
       })
     end,
   },
